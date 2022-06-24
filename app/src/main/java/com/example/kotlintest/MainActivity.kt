5f8a0c6b0469582dc9afa12359fb13a12e225acd
SorokinE757 <dragon40in@gmail.com>
2022-06-24 16:49:05 +0300

    Добавлена Button с листнером.

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.kotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.kotlintest.R
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.do_dz_btn).setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
