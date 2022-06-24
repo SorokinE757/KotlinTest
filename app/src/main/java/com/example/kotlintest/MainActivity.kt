a943c4df8c8d05b8e6f9600bdda454f3c3a02ffd
SorokinE757 <dragon40in@gmail.com>
2022-06-24 17:24:22 +0300

    Создан data class, 2 его объекта, второй при помощи .copy, и их значения выведены на экран.

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.kotlintest
 
+import android.app.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.example.kotlintest.R
+import android.widget.TextView
 
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.do_dz_btn).setOnClickListener(object: View.OnClickListener{
-            override fun onClick(p0: View?) {
-                TODO("Not yet implemented")
-            }
-        })
+        findViewById<Button>(R.id.do_dz_btn).setOnClickListener(this)
+    }
+    private val firstPerson = Person("Евгений", "Сорокин")
+    private val secondPerson = firstPerson.copy(name = "Александр")
+
+    override fun onClick(p0: View?) {
+        findViewById<TextView>(R.id.tf1).text = firstPerson.name
+        findViewById<TextView>(R.id.tf2).text = firstPerson.surname
+        findViewById<TextView>(R.id.tf3).text = secondPerson.name
+        findViewById<TextView>(R.id.tf4).text = secondPerson.surname
     }
 }
